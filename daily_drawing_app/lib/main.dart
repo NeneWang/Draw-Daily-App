051f9f678678fbd769842ab8531105799e057c90
NelsonWang <wangnelson2@gmail.com>
2021-06-14 11:00:51 -0400

    Setup | Core | renaming

--- daily_drawing_app/lib/main.dart
+++ daily_drawing_app/lib/main.dart
@@ -3,9 +3,9 @@ import 'package:provider/provider.dart';
 
 import './providers/great_places.dart';
 
-import './screens/add_place_screen.dart';
+import 'screens/add_images_screen.dart';
 import './screens/places_list_screen.dart';
-import './screens/place_detail_screen.dart';
+import 'screens/image_detail_screen.dart';
 
 void main() => runApp(MyApp());
 
