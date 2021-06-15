e4b4595afba627f3d041a878f2dc451583b316ed
NelsonWang <wangnelson2@gmail.com>
2021-06-14 21:40:25 -0400

    Setup | Core | calendar

--- daily_drawing_app/lib/screens/calendar_screen.dart
+++ daily_drawing_app/lib/screens/calendar_screen.dart
@@ -24,42 +24,7 @@ static const routeName = '/calendar';
             ),
           ],
         ),
-        body: FutureBuilder(
-          future: Provider.of<GreatPlaces>(context, listen: false)
-              .fetchAndSetImages(),
-          builder: (ctx, snapshot) => snapshot.connectionState ==
-                  ConnectionState.waiting
-              ? Center(
-                  child: CircularProgressIndicator(),
-                )
-              : Consumer<GreatPlaces>(
-                  child: Center(
-                    child: const Text('Got no places yet, start adding some!'),
-                  ),
-                  builder: (ctx, greatPlaces, ch) =>
-                      greatPlaces.items.length <= 0
-                          ? ch
-                          : ListView.builder(
-                              itemCount: greatPlaces.items.length,
-                              itemBuilder: (ctx, i) => ListTile(
-                                leading: CircleAvatar(
-                                  backgroundImage: FileImage(
-                                    greatPlaces.items[i].image,
-                                  ),
-                                ),
-                                title: Text(greatPlaces.items[i].title),
-                                subtitle: Text(greatPlaces.items[i].dateTime
-                                    .toIso8601String()),
-                                onTap: () {
-                                  Navigator.of(context).pushNamed(
-                                    PlaceDetailScreen.routeName,
-                                    arguments: greatPlaces.items[i].id,
-                                  );
-                                },
-                              ),
-                            ),
-                ),
-        ),
+        body: Column(),
         bottomNavigationBar: BottomNavigator(1, context));
   }
 }
