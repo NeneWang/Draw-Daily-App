4f8063931184a3263380920510f23d291f66839a
NelsonWang <wangnelson2@gmail.com>
2021-06-15 17:08:06 -0400

    Setup | Core | Calendar Screen

--- daily_drawing_app/lib/screens/calendar_screen.dart
+++ daily_drawing_app/lib/screens/calendar_screen.dart
@@ -1,33 +1,62 @@
-import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-
-import 'add_images_screen.dart';
-import '../providers/images_provider.dart';
-import 'image_detail_screen.dart';
+// Copyright 2019 Aleksander Woźniak
+// SPDX-License-Identifier: Apache-2.0
 
 import 'dart:collection';
+
+import 'package:flutter/material.dart';
 import 'package:table_calendar/table_calendar.dart';
+
+import 'utils.dart';
+
+import 'add_images_screen.dart';
 
 import '../widgets/bottom_navigator.dart';
 
-import 'pages/basics_example.dart';
-// import 'pages/complex_example.dart';
-// import 'pages/events_example.dart';
-import 'pages/multi_example.dart';
-import 'pages/range_example.dart';
-
-
-class Event {
-  final String title;
-
-  const Event(this.title);
-
+class CalendarScreen extends StatefulWidget {
+  static const routeName = '/calendar';
   @override
-  String toString() => title;
+  _CalendarScreenState createState() => _CalendarScreenState();
 }
 
-class CalendarScreen extends StatelessWidget {
-  static const routeName = '/calendar';
+class _CalendarScreenState extends State<CalendarScreen> {
+  final ValueNotifier<List<Event>> _selectedEvents = ValueNotifier([]);
+
+  // Using a `LinkedHashSet` is recommended due to equality comparison override
+  final Set<DateTime> _selectedDays = LinkedHashSet<DateTime>(
+    equals: isSameDay,
+    hashCode: getHashCode,
+  );
+
+  CalendarFormat _calendarFormat = CalendarFormat.month;
+  DateTime _focusedDay = DateTime.now();
+
+  @override
+  void dispose() {
+    _selectedEvents.dispose();
+    super.dispose();
+  }
+
+  List<Event> _getEventsForDay(DateTime day) {
+    // Implementation example
+    return kEvents[day] ?? [];
+  }
+
+  List<Event> _getEventsForDays(Set<DateTime> days) {
+    // Implementation example
+    // Note that days are in selection order (same applies to events)
+    return [
+      for (final d in days) ..._getEventsForDay(d),
+    ];
+  }
+
+  void _onDaySelected(DateTime selectedDay, DateTime focusedDay) {
+    setState(() {
+      _selectedDays.clear();
+      _selectedDays.add(selectedDay);
+    });
+
+    _selectedEvents.value = _getEventsForDays(_selectedDays);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -43,38 +72,59 @@ class CalendarScreen extends StatelessWidget {
             ),
           ],
         ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const SizedBox(height: 20.0),
-              ElevatedButton(
-                child: Text('Basics'),
-                onPressed: () => Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (_) => TableBasicsExample()),
-                ),
+        body: Column(
+          children: [
+            TableCalendar<Event>(
+              firstDay: kFirstDay,
+              lastDay: kLastDay,
+              focusedDay: _focusedDay,
+              calendarFormat: _calendarFormat,
+              eventLoader: _getEventsForDay,
+              startingDayOfWeek: StartingDayOfWeek.monday,
+              selectedDayPredicate: (day) {
+                // Use values from Set to mark multiple days as selected
+                return _selectedDays.contains(day);
+              },
+              onDaySelected: _onDaySelected,
+              onFormatChanged: (format) {
+                if (_calendarFormat != format) {
+                  setState(() {
+                    _calendarFormat = format;
+                  });
+                }
+              },
+              onPageChanged: (focusedDay) {
+                _focusedDay = focusedDay;
+              },
+            ),
+            const SizedBox(height: 8.0),
+            Expanded(
+              child: ValueListenableBuilder<List<Event>>(
+                valueListenable: _selectedEvents,
+                builder: (context, value, _) {
+                  return ListView.builder(
+                    itemCount: value.length,
+                    itemBuilder: (context, index) {
+                      return Container(
+                        margin: const EdgeInsets.symmetric(
+                          horizontal: 12.0,
+                          vertical: 4.0,
+                        ),
+                        decoration: BoxDecoration(
+                          border: Border.all(),
+                          borderRadius: BorderRadius.circular(12.0),
+                        ),
+                        child: ListTile(
+                          onTap: () => print('${value[index]}'),
+                          title: Text('${value[index]}'),
+                        ),
+                      );
+                    },
+                  );
+                },
               ),
-              const SizedBox(height: 12.0),
-              ElevatedButton(
-                child: Text('Range Selection'),
-                onPressed: () => Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (_) => TableRangeExample()),
-                ),
-              ),
-              const SizedBox(height: 12.0),
-              ElevatedButton(
-                child: Text('Multiple Selection'),
-                onPressed: () => Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (_) => TableMultiExample()),
-                ),
-              ),
-              const SizedBox(height: 12.0),
-              const SizedBox(height: 20.0),
-            ],
-          ),
+            ),
+          ],
         ),
         bottomNavigationBar: BottomNavigator(1, context));
   }
--- daily_drawing_app/lib/screens/pages/multi_example.dart
+++ daily_drawing_app/lib/screens/pages/multi_example.dart
@@ -8,6 +8,10 @@ import 'package:table_calendar/table_calendar.dart';
 
 import '../utils.dart';
 
+import '../add_images_screen.dart';
+
+import '../../widgets/bottom_navigator.dart';
+
 class TableMultiExample extends StatefulWidget {
   @override
   _TableMultiExampleState createState() => _TableMultiExampleState();
@@ -46,17 +50,8 @@ class _TableMultiExampleState extends State<TableMultiExample> {
 
   void _onDaySelected(DateTime selectedDay, DateTime focusedDay) {
     setState(() {
-      _focusedDay = focusedDay;
-      // Update values in a Set
-      // if (_selectedDays.contains(selectedDay)) {
-      //   _selectedDays.remove(selectedDay);
-      // } else {
-      //   _selectedDays.add(selectedDay);
-      // }
-      // print(_selectedDays);
       _selectedDays.clear();
-      _selectedDays .add(selectedDay);
-
+      _selectedDays.add(selectedDay);
     });
 
     _selectedEvents.value = _getEventsForDays(_selectedDays);
@@ -65,72 +60,71 @@ class _TableMultiExampleState extends State<TableMultiExample> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('TableCalendar - Multi'),
-      ),
-      body: Column(
-        children: [
-          TableCalendar<Event>(
-            firstDay: kFirstDay,
-            lastDay: kLastDay,
-            focusedDay: _focusedDay,
-            calendarFormat: _calendarFormat,
-            eventLoader: _getEventsForDay,
-            startingDayOfWeek: StartingDayOfWeek.monday,
-            selectedDayPredicate: (day) {
-              // Use values from Set to mark multiple days as selected
-              return _selectedDays.contains(day);
-            },
-            onDaySelected: _onDaySelected,
-            onFormatChanged: (format) {
-              if (_calendarFormat != format) {
-                setState(() {
-                  _calendarFormat = format;
-                });
-              }
-            },
-            onPageChanged: (focusedDay) {
-              _focusedDay = focusedDay;
-            },
-          ),
-          ElevatedButton(
-            child: Text('Clear selection'),
-            onPressed: () {
-              setState(() {
-                _selectedDays.clear();
-                _selectedEvents.value = [];
-              });
-            },
-          ),
-          const SizedBox(height: 8.0),
-          Expanded(
-            child: ValueListenableBuilder<List<Event>>(
-              valueListenable: _selectedEvents,
-              builder: (context, value, _) {
-                return ListView.builder(
-                  itemCount: value.length,
-                  itemBuilder: (context, index) {
-                    return Container(
-                      margin: const EdgeInsets.symmetric(
-                        horizontal: 12.0,
-                        vertical: 4.0,
-                      ),
-                      decoration: BoxDecoration(
-                        border: Border.all(),
-                        borderRadius: BorderRadius.circular(12.0),
-                      ),
-                      child: ListTile(
-                        onTap: () => print('${value[index]}'),
-                        title: Text('${value[index]}'),
-                      ),
-                    );
-                  },
-                );
+        appBar: AppBar(
+          title: Text('View Progress'),
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.add_a_photo),
+              onPressed: () {
+                Navigator.of(context).pushNamed(AddPlaceScreen.routeName);
               },
             ),
-          ),
-        ],
-      ),
-    );
+          ],
+        ),
+        body: Column(
+          children: [
+            TableCalendar<Event>(
+              firstDay: kFirstDay,
+              lastDay: kLastDay,
+              focusedDay: _focusedDay,
+              calendarFormat: _calendarFormat,
+              eventLoader: _getEventsForDay,
+              startingDayOfWeek: StartingDayOfWeek.monday,
+              selectedDayPredicate: (day) {
+                // Use values from Set to mark multiple days as selected
+                return _selectedDays.contains(day);
+              },
+              onDaySelected: _onDaySelected,
+              onFormatChanged: (format) {
+                if (_calendarFormat != format) {
+                  setState(() {
+                    _calendarFormat = format;
+                  });
+                }
+              },
+              onPageChanged: (focusedDay) {
+                _focusedDay = focusedDay;
+              },
+            ),
+            const SizedBox(height: 8.0),
+            Expanded(
+              child: ValueListenableBuilder<List<Event>>(
+                valueListenable: _selectedEvents,
+                builder: (context, value, _) {
+                  return ListView.builder(
+                    itemCount: value.length,
+                    itemBuilder: (context, index) {
+                      return Container(
+                        margin: const EdgeInsets.symmetric(
+                          horizontal: 12.0,
+                          vertical: 4.0,
+                        ),
+                        decoration: BoxDecoration(
+                          border: Border.all(),
+                          borderRadius: BorderRadius.circular(12.0),
+                        ),
+                        child: ListTile(
+                          onTap: () => print('${value[index]}'),
+                          title: Text('${value[index]}'),
+                        ),
+                      );
+                    },
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+        bottomNavigationBar: BottomNavigator(1, context));
   }
 }
